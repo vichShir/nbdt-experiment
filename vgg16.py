c5e894bfc57205ed76fb5cbbf1ad821e06c7fea7
vichShir <victor.shirasuna@gmail.com>
2024-07-03 18:20:58 -0300

    Updated script experiment

--- vgg16.py
+++ vgg16.py
@@ -1,104 +1,22 @@
+import numpy as np
 import torch.nn as nn
 import torch
-import torchvision
-import torchvision.transforms as transforms
-from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
 from nbdt.model import SoftNBDT
 from nbdt.loss import SoftTreeSupLoss
 from nbdt.hierarchy import generate_hierarchy
 from nbdt.tree import Tree
-from tqdm import tqdm
-
-
-def pretrain(net, trainloader, optimizer, criterion, epochs=10, device='cuda'):
-    net.train()
-    for epoch in range(epochs):
-        for batch_idx, (inputs, targets) in enumerate(tqdm(trainloader)):
-            inputs, targets = inputs.to(device), targets.to(device)
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-
-
-def train_nbdt(net, trainloader, optimizer, criterion, epochs=10, device='cuda'):
-    net.train()
-    for epoch in range(epochs):
-        for batch_idx, (inputs, targets) in enumerate(tqdm(trainloader)):
-            inputs, targets = inputs.to(device), targets.to(device)
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-
-
-def test(net, testloader, device='cuda'):
-    net.eval()
-    with torch.no_grad():
-        pred_classes = []
-        test_targets = []
-        for batch_idx, (inputs, targets) in enumerate(tqdm(testloader)):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = torch.argmax(net(inputs), dim=1)
-            pred_classes.append(outputs)
-            test_targets.append(targets)
-
-        acc = accuracy_score(torch.cat(test_targets).cpu().numpy(), torch.cat(pred_classes).cpu().numpy())
-        return acc
-
-
-def load_vgg16(num_classes=1000):
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg16', pretrained=True)
-    mod = list(model.classifier.children())
-    mod.pop()
-    mod.append(torch.nn.Linear(4096, num_classes))
-    new_classifier = torch.nn.Sequential(*mod)
-    model.classifier = new_classifier
-    return model
-
-
-def load_data():
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ]
-    )
-    trainset = torchvision.datasets.CIFAR10(
-        root='./data', 
-        train=True,
-        download=True, 
-        transform=transform
-    )
-    testset = torchvision.datasets.CIFAR10(
-        root='./data', 
-        train=False,
-        download=False, 
-        transform=transform
-    )
-    trainloader = torch.utils.data.DataLoader(
-        trainset, 
-        batch_size=128,
-        shuffle=True, 
-        num_workers=2
-    )
-    testloader = torch.utils.data.DataLoader(
-        testset, 
-        batch_size=128,
-        shuffle=False, 
-        num_workers=2
-    )
-    return (trainloader, testloader)
+from PIL import Image
+from utils import train, test, load_vgg16, load_data, plot_decision_tree
 
 
 def main():
     torch.manual_seed(0)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # load model
-    model = load_vgg16(num_classes=10)
-    model = model.cuda()
+    model = load_vgg16(num_classes=10).to(device)
 
     # load data
     classes = (
@@ -108,8 +26,8 @@ def main():
     trainloader, testloader = load_data()
 
     # pretrain model
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
-    pretrain(model, trainloader, optimizer, nn.CrossEntropyLoss(), epochs=5)
+    optimizer = torch.optim.SGD(model.classifier[-1].parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
+    train(model, trainloader, optimizer, nn.CrossEntropyLoss(), epochs=3, device=device)
 
     # generate hierarchy from pretrained model
     generate_hierarchy(dataset='CIFAR10', arch='vgg16', model=model, method='induced')
@@ -119,28 +37,24 @@ def main():
     tree.visualize('./out/CIFAR10-induced-VGG16-tree.html', dataset='CIFAR10')
 
     # fine-tune model with tree supervision loss
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
     criterion = nn.CrossEntropyLoss()
     criterion = SoftTreeSupLoss(dataset='CIFAR10', hierarchy='induced-vgg16', criterion=criterion)
-    train_nbdt(model, trainloader, optimizer, criterion, epochs=20)
+    train(model, trainloader, optimizer, criterion, epochs=2, device=device)
 
     # run inference using embedded decision rules
-    model = SoftNBDT(model=model, dataset='CIFAR10', hierarchy='induced-vgg16')
-    model.eval()
+    model_nbdt = SoftNBDT(model=model, dataset='CIFAR10', hierarchy='induced-vgg16')
+    model_nbdt.eval()
 
-    # test set
-    test_acc = test(model, testloader)
+    # evaluate on test set
+    test_acc = test(model_nbdt, testloader)
     print('Test accuracy:', test_acc)
 
-    # individual sample with decisions
-    b = next(iter(testloader))
-    X = b[0][0].unsqueeze(0).to('cuda')
-    y = b[1][0]
-
-    # print(model.forward_with_decisions(X))
-    print('true label:', classes[y])
-
-    for node in model.forward_with_decisions(X)[1][0]:
-        print(node)
+    # save checkpoint
+    torch.save(
+        {'state_dict': model_nbdt.state_dict()}, 
+        './SoftNBDT_model.pt'
+    )
 
 
 if __name__ == '__main__':
